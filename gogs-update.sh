741e74d34b41b1dbf5d3bd01bbe720e0a6ab4cba
nightBulb <gwibo.web@gmail.com>
2017-03-10 02:59:45 +0530

    Various bug fixes
     - Delete gogs_old only if user data exists in current gogs folder
     - run shell commands(mv, rm, cp...) as "git" user
     - Don't backup gogs folder if its withou user data
     - change function name update()->doUpdate()

--- gogs-update.sh
+++ gogs-update.sh
@@ -17,7 +17,11 @@ loge () {
 
 preUpdate () {
 	local LOGTAG="preUpdate"
+	
 	local GOGS_PARENT_DIR=$1
+	local GOGS_USER=$2
+
+	local RUN_AS_USER="sudo -u $GOGS_USER"
 
 	logd $LOGTAG "Gogs Parent DIR:$GOGS_PARENT_DIR"
 	
@@ -30,52 +34,66 @@ preUpdate () {
 		
 		if [ -d $GOGS_PARENT_DIR/gogs_old ]; then
 			logd $LOGTAG "Existing gogs_old directory Found"
-			rm -r gogs_old
+			if [ -d $GOGS_PARENT_DIR/gogs/custom ] && [ -d $GOGS_PARENT_DIR/gogs/data ] && [ -d $GOGS_PARENT_DIR/gogs/log ]; then
+				logd $LOGTAG "User data for Gogs exists in current gogs folder"
+				logd $LOGTAG "Deleting old backup folder"
+				$RUN_AS_USER rm -r $GOGS_PARENT_DIR/gogs_old
+
+				logd $LOGTAG "Moving gogs to gogs_old"
+				$RUN_AS_USER mv $GOGS_PARENT_DIR/gogs $GOGS_PARENT_DIR/gogs_old
+			else
+				logw $LOGTAG "User data does not exist in current gogs folder"
+				$RUN_AS_USER rm -r $GOGS_PARENT_DIR/gogs
+
+			fi
+	
 		else
 			logw $LOGTAG "No pre-existing/backup gogs_old directory found"
 	        fi
 		
-		logd $LOGTAG "Moving gogs to gogs_old"
-		mv $GOGS_PARENT_DIR/gogs $GOGS_PARENT_DIR/gogs_old
 
-	elif [ -d $GOG_PARENT_DIR/gogs_old ]; then
+	elif [ -d $GOGS_PARENT_DIR/gogs_old ]; then
 		logd $LOGTAG "No existing Gogs DIR but old/backup exists"
 		return 0
 	else
 		logw $LOGTAG "No Existing Gogs Directory Found"
 		return 1
-
 	fi
 
 }
 
-update () {
+doUpdate () {
 	local LOGTAG="update"
+	
 	local GOGS_PARENT_DIR=$1
 	local NEW_GOGS_ZIP=$2
+	local GOGS_USER=$3
 	
 
 	logd $LOGTAG "Unzipping new gogs"
-
-	unzip $NEW_GOGS_ZIP -d $GOGS_PARENT_DIR
+	sudo -u $GOGS_USER unzip $NEW_GOGS_ZIP -d $GOGS_PARENT_DIR
 }
 
 postUpdate () {
 	local LOGTAG="postUpdate"
 
 	local GOGS_PARENT_DIR=$1
-	logd $LOGTAG "Gogs Dir:$GOGS_PARENT_DIR"
+	local GOGS_USER=$2
+
+	local RUN_AS_USER="sudo -u $GOGS_USER"
+
+	logd $LOGTAG "Gogs Dir:$GOGS_PARENT_DIR, Gogs User:$GOGS_USER"
 	
 	if [ -d $GOGS_PARENT_DIR/gogs ]; then
 	
 		logd $LOGTAG "Gogs Directory found, looking for old directory"
 
 		if [ -d $GOGS_PARENT_DIR/gogs_old ]; then
-			log $LOGTAG "Old Gogs Directory Found, Copying old data"
+			logd $LOGTAG "Old Gogs Directory Found, Copying old data"
 			
-			cp -R $GOGS_PARENT_DIR/gogs_old/custom gogs
-			cp -R $GOGS_PARENT_DIR/gogs_old/data gogs
-			cp -R $GOGS_PARENT_DIR/gogs_old/log gogs
+			$RUN_AS_USER cp -R $GOGS_PARENT_DIR/gogs_old/custom 	$GOGS_PARENT_DIR/gogs
+			$RUN_AS_USER cp -R $GOGS_PARENT_DIR/gogs_old/data 	$GOGS_PARENT_DIR/gogs
+			$RUN_AS_USER cp -R $GOGS_PARENT_DIR/gogs_old/log 	$GOGS_PARENT_DIR/gogs
 		else
 			loge $LOGTAG "NOT FOUND: Old gogs directory, either this is first time installation or something went wrong during update and old data was not backed up"
 		fi
@@ -95,22 +113,24 @@ main () {
 	
 	local GOGS_PARENT_DIR=$1
 	local UPDATE_ZIP_FILE=$2
+
+	local GOGS_USER="git"
 	
 	printUsage
         logd $LOGTAG "Starting Gogs update"
 
 
 	
-	preUpdate $GOGS_PARENT_DIR
+	preUpdate $GOGS_PARENT_DIR $GOGS_USER
 	
 	if [ $? -ne 0 ]; then
 		loge $LOGTAG "Pre Update Failed"
 		exit 1
 	fi
 
-	update $GOGS_PARENT_DIR $UPDATE_ZIP_FILE
+	doUpdate $GOGS_PARENT_DIR $UPDATE_ZIP_FILE $GOGS_USER
 
-	postUpdate $GOGS_PARENT_DIR
+	postUpdate $GOGS_PARENT_DIR $GOGS_USER
 }
 
 main $1 $2
